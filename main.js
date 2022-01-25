0e14a32b03a1fc3084e2d176b07a8c62708d22dd
HZie <boss.hz734@gmail.com>
2022-01-25 11:58:53 +0900

    js: handle control button

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const carrots = document.querySelectorAll('.carrot');
 carrots.forEach((carrot) => {
   const x = Math.random() * 90;
   const y = Math.random() * 70;
-  console.log(`${x}, ${y}`);
   carrot.style.left = `${x}%`;
   carrot.style.top = `${y}%`;
 });
@@ -14,7 +13,30 @@ const bugs = document.querySelectorAll('.bug');
 bugs.forEach((bug) => {
   const x = Math.random() * 90;
   const y = Math.random() * 70;
-  console.log(`${x}, ${y}`);
   bug.style.left = `${x}%`;
   bug.style.top = `${y}%`;
 });
+
+const gameControlBtn = document.querySelector('.game__control-btn');
+const pause = document.querySelector('.fa-pause');
+const play = document.querySelector('.fa-play');
+
+gameControlBtn.addEventListener('click', () => {
+  pause.classList.toggle('active');
+  play.classList.toggle('active');
+
+  // when pressed play button
+  if (pause.classList.contains('active')) {
+    startGame();
+  } else {
+    initGame();
+  }
+});
+
+function startGame() {
+  console.log('start game');
+}
+
+function initGame() {
+  console.log('init game');
+}
